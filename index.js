76326a51b854caeca8444339304624c45160baf2
Abenezer <abentad4@gmail.com>
2021-01-10 17:15:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const { response } = require("express");
+
+const url = "https://r6.tracker.network/profile/pc/";
+
+//express stuff
+app.get("/api/:name", (req, res) => {
+  findBaseInfo(req.params.name).then((body) => {
+    const $ = cheerio.load(body);
+    //user name
+    const name = $(".trn-profile-header__name > span:first-child").text();
+    //profile views
+    const proView = $(".trn-profile-header__name > span:nth-child(2)").text();
+    //level
+    const level = $(".trn-defstat__data .trn-defstat__value")
+      .eq(0)
+      .text()
+      .trim();
+    //best mmr rating
+    const mmrRating = $(".trn-defstat__data .trn-defstat__value")
+      .eq(1)
+      .text()
+      .trim();
+    //rank text
+    const rankText = $(".trn-defstat__value").eq(2).text().trim();
+    //average seasonal mmr
+    const avgMmr = $(".trn-defstat__value").eq(3).text().trim();
+    //one top operator
+    const topOp = $(".top-operators .trn-defstat__value img:nth-child(1)")
+      .attr("src")
+      .trim();
+    //
+    const info = {
+      name,
+      proView,
+      level,
+      mmrRating,
+      rankText,
+      avgMmr,
+      topOp,
+    };
+    res.json(info);
+  });
+});
+//
+//
+function findBaseInfo(searchTerm) {
+  return fetch(`${url}${searchTerm}`).then((response) => response.text());
+}
+//
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
